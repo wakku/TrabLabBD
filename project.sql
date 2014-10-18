5dc9d51511fc3e9063a3b8fbcd44faab25d363ef
arthbr <arthbr@gmail.com>
2014-10-18 18:25:47 -0300

    Mudança de alguns tipos; inclusões de chaves estrangeiras

--- project.sql
+++ project.sql
@@ -1,17 +1,17 @@
-CREATE SEQUENCE USUARIO_ID_USUARIO_SEQ;
-CREATE SEQUENCE CLIENTE_ID_CLIENTE_SEQ;
-CREATE SEQUENCE COMPRA_ID_COMPRA_SEQ;
-CREATE SEQUENCE ENDERECO_ID_ENDERECO_SEQ;
-CREATE SEQUENCE FRETE_ID_FRETE_SEQ;
-CREATE SEQUENCE VISUALIZACAO_ID_VISUALIZACAO_SEQ;
-CREATE SEQUENCE COMENTARIO_ID_COMENTARIO_SEQ;
-CREATE SEQUENCE AVALIACAO_ID_AVALIACAO_SEQ;
-CREATE SEQUENCE PRODUTO_ID_PRODUTO_SEQ;
-CREATE SEQUENCE PRODUTO_FORNECEDOR_ID_PRODUTO_FORNECEDOR_SEQ;
-CREATE SEQUENCE FORNECEDOR_ID_FORNECEDOR_SEQ;
-CREATE SEQUENCE CATEGORIA_ID_CATEGORIA_SEQ;
-CREATE SEQUENCE PRECO_ID_PRECO_SEQ;
-CREATE SEQUENCE PROMOCAO_ID_PROMOCAO_SEQ;
+create sequence USUARIO_ID_USUARIO_SEQ;
+create sequence CLIENTE_ID_CLIENTE_SEQ;
+create sequence COMPRA_ID_COMPRA_SEQ;
+create sequence ENDERECO_ID_ENDERECO_SEQ;
+create sequence FRETE_ID_FRETE_SEQ;
+create sequence VISUALIZACAO_ID_VISUALIZACAO_SEQ;
+create sequence COMENTARIO_ID_COMENTARIO_SEQ;
+create sequence AVALIACAO_ID_AVALIACAO_SEQ;
+create sequence PRODUTO_ID_PRODUTO_SEQ;
+create sequence PRODUTO_FORNECEDOR_ID_PRODUTO_FORNECEDOR_SEQ;
+create sequence FORNECEDOR_ID_FORNECEDOR_SEQ;
+create sequence CATEGORIA_ID_CATEGORIA_SEQ;
+create sequence PRECO_ID_PRECO_SEQ;
+create sequence PROMOCAO_ID_PROMOCAO_SEQ;
 
 --DROP TABLE CLIENTE_ID_CLIENTE_SEQ CASCADE;
 --DROP TABLE COMPRA_ID_COMPRA_SEQ CASCADE;
@@ -28,86 +28,88 @@ CREATE SEQUENCE PROMOCAO_ID_PROMOCAO_SEQ;
 --DROP TABLE PROMOCAO_ID_PROMOCAO_SEQ CASCADE;
 
 create table USUARIO(
-    id_usuario integer DEFAULT NEXTVAL ('USUARIO_ID_USUARIO_SEQ') PRIMARY KEY,
-  nome_cliente varchar(70) NOT NULL,
-  cpf char(11) NOT NULL UNIQUE,
-  telefone varchar (15),
-  email varchar (40),    
+	id_usuario int primary key default NEXTVAL ('USUARIO_ID_USUARIO_SEQ'),
+	nome_cliente varchar(70) not null,
+	cpf char(11) not null unique,
+	telefone varchar (15),
+	email varchar(40),		
 );
 
 create table ADMIN(
-    id_admin int
+	id_admin int 
 );
 
 create table CLIENTE(
-  id_cliente integer PRIMARY KEY DEFAULT NEXTVAL ('CLIENTE_ID_CLIENTE_SEQ'),
-  id_endereco integer REFERENCES endereco(id_endereco) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE
+	id_cliente int primary key references usuario(id_usuario) on update cascade on delete cascade,
+	id_endereco int REFERENCES endereco(id_endereco) not null on update cascade on delete cascade
 );
 
 create table ENDERECO(
-  id_endereco integer PRIMARY KEY DEFAULT NEXTVAL ('ENDERECO_ID_ENDERECO_SEQ'),
-  logradouro varchar(50) NOT NULL,
-  numero varchar(6) NOT NULL,
-  complemento varchar(30),
-  cep char(8) NOT NULL,
-  cidade varchar(30) NOT NULL,
-  estado char(2) NOT NULL
+	id_endereco int primary key default NEXTVAL ('ENDERECO_ID_ENDERECO_SEQ'),
+	logradouro varchar(50) not null,
+	numero varchar(6) not null,
+	complemento varchar(30),
+	cep char(8) not null,
+	cidade varchar(30) not null,
+	estado char(2) not null
 );
 
 create table FRETE(
-  id_frete integer DEFAULT NEXTVAL ('FRETE_ID_FRETE_SEQ')PRIMARY KEY,
-  cep_inicio char(8) NOT NULL,
-  cep_final char(8) NOT NULL,
-  preco integer DEFAULT 0
+	id_frete int primary key default NEXTVAL ('FRETE_ID_FRETE_SEQ'),
+	cep_inicio char(8) not null,
+	cep_final char(8) not null,
+	preco int default 0
 );
 
 create table COMPRA(
-  id_venda integer DEFAULT NEXTVAL ('COMPRA_ID_COMPRA_SEQ')PRIMARY KEY,
-  id_cliente integer REFERENCES cliente(id_cliente) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
-  id_frete integer REFERENCES cliente(id_cliente) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
-  produtos integer[] NOT NULL,
-  dia_hora timestamp DEFAULT now()
+	id_venda int default NEXTVAL ('COMPRA_ID_COMPRA_SEQ')primary key,
+	id_cliente int REFERENCES cliente(id_cliente) not null on update cascade on delete cascade,
+	id_frete int REFERENCES cliente(id_cliente) not null on update cascade on delete cascade,
+	produtos int[] not null,
+	dia_hora timestamp default now()
 );
 
 create table VISUALIZACAO(
-  id_visualizacao bigint DEFAULT NEXTVAL ('VISUALIZACAO_ID_VISUALIZACAO_SEQ') PRIMARY KEY,
-  id_cliente integer REFERENCES cliente(id_cliente) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
-  id_produto integer REFERENCES produto(id_produto) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
-  dia_hora timestamp DEFAULT now()
+	id_visualizacao bigint default NEXTVAL ('VISUALIZACAO_ID_VISUALIZACAO_SEQ') primary key,
+	id_cliente int REFERENCES cliente(id_cliente) not null on update cascade on delete cascade,
+	id_produto int REFERENCES produto(id_produto) not null on update cascade on delete cascade,
+	dia_hora timestamp default now()
 );
 
 create table AVALIACAO(
-  id_avaliacao integer DEFAULT NEXTVAL('AVALIACAO_ID_AVALIACAO_SEQ') PRIMARY KEY,
-  id_cliente integer REFERENCES cliente(id_cliente) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
-  id_produto integer REFERENCES produto(id_produto) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
-  nota smallint check (nota > 0) check (nota < 5),
-  comentario varchar(1000)
+	id_avaliacao int default NEXTVAL('AVALIACAO_ID_AVALIACAO_SEQ') primary key,
+	id_cliente int REFERENCES cliente(id_cliente) not null on update cascade on delete cascade,
+	id_produto int REFERENCES produto(id_produto) not null on update cascade on delete cascade,
+	nota smallint check (nota between 0 and 5),
+	comentario varchar(1000)
 );
 
 create table PRODUTO(
-  id_produto integer DEFAULT NEXTVAL('PRODUTO_ID_PRODUTO_SEQ') PRIMARY KEY,
-  imagem oid,
-  categoria varchar(100) NOT NULL,
-  em_estoque integer check (em_estoque > 0),
-  minimo_em_estoque integer check (minimo_em_estoque > 0),
-  maximo_em_estoque integer check (maximo_em_estoque > 0),
-  preco integer NOT NULL check(preco > 0)
+	id_produto int primary key default NEXTVAL('PRODUTO_ID_PRODUTO_SEQ'),
+	imagem oid,
+	categoria varchar(100) not null,
+	em_estoque int check (em_estoque > 0),
+	estoque int check (estoque > 0),
+	preco decimal(5,2) not null check (preco > 0)
 );
 
 create table LIVRO(
-    editora varchar(80) not null
+	id_livro int primary key references produto(id_produto),
+	editora varchar(80) not null
 );
 
 create table CD(
-    gravadora varchar(80) not null
+	id_cd int primary key references produto(id_produto),
+	gravadora varchar(80) not null
 );
 
 create table DVD(
-    classificacao int
+	id_dvd int primary key references produto(id_produto),
+	classificacao int
 );
 
 create table PROMOCAO(
-  id_promocao bigint DEFAULT NEXTVAL ('PROMOCAO_ID_PROMOCAO_SEQ') PRIMARY KEY,
-  id_produto integer REFERENCES produto(id_produto) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
-  preco integer NOT NULL check (preco > 0)
+	id_promocao bigint primary key default NEXTVAL ('PROMOCAO_ID_PROMOCAO_SEQ'),
+	id_produto int REFERENCES produto(id_produto) not null on update cascade on delete cascade,
+	preco int not null check (preco > 0)
 );
